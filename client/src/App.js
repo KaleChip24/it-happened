35270d3232d222b2bf3eef70ce4c03d32005867c
KaleOHara <92695012+KaleChip24@users.noreply.github.com>
2021-12-02 12:51:20 -0500

    nav added, data is being pulled from the API via consolelog atm. setting up components folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,32 @@
 import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react'
+import { grabFilms } from './services';
+import { Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar';
 
 function App() {
+  const [films, setFilms] = useState([])
+
+  useEffect(() => {
+    const getAllFilms = async () => {
+      const res = await grabFilms()
+      console.log(res)
+      setFilms(res)
+    }
+    getAllFilms()
+  }, [])
+
+
   return (
     <div className="App">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={null} />
+        <Route path="/new" element={null} />
+        <Route path="/films/:id" element={null} />
+        <Route path="/edit/:id" element={null} />
+      </Routes>
 
     </div>
   );
---
+++ client/src/services/index.js
@@ -0,0 +1,13 @@
+import axios from "axios"
+
+export const BASE_URL = `https://api.airtable.com/v0/${process.env.REACT_APP_API_BASE}/films`
+export const config = {
+  headers: {
+    Authorization: `Bearer ${process.env.REACT_APP_API_KEY}`
+  }
+}
+
+export const grabFilms = async () => {
+  const res = await axios.get(BASE_URL, config)
+  return res.data.records
+}
