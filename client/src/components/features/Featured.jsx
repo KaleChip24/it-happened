9bd7ba5c89dbdc87bccbffe9e7840c1b93808619
KaleOHara <92695012+KaleChip24@users.noreply.github.com>
2021-12-04 13:42:50 -0500

    found another way that the carousel doesnt work

--- client/src/components/features/Featured.jsx
+++ client/src/components/features/Featured.jsx
@@ -1,24 +1,53 @@
 import { Link } from 'react-router-dom'
+import { useEffet, useState } from 'react'
 import Carousel from 'react-bootstrap/Carousel';
 
 function Featured(props) {
+
   return (
     <div>
-      <Carousel variant="dark">
-        <Carousel.Item>
-          {
-            props.features.map(feature => (
-              <div className="cats" key={feature.id}>
-                <Link
-                  to={`/featured/${feature.id}`}>{<img style={{ width: 400, height: 300 }} src={feature.fields.image} alt="Cat" />}</Link>
-                <h2>{feature.fields.name}</h2>
-              </div>
-            ))
-          }
-        </Carousel.Item>
-      </Carousel>
+      {
+        props.features.map(feature => (
+          <div className="cats" key={feature.id}>
+            <Link
+              to={`/featured/${feature.id}`}>{<img style={{ width: 400, height: 300 }} src={feature.fields.image} alt="Cat" />}</Link>
+            <h2>{feature.fields.name}</h2>
+          </div>
+        ))
+      }
     </div>
   );
 }
 
 export default Featured;
+
+
+
+{/* <Carousel variant="dark">
+  <Carousel.Item>
+    {
+      props.features.map(feature => (
+        <div className="cats" key={feature.id}>
+          <Link
+            to={`/featured/${feature.id}`}>{<img style={{ width: 400, height: 300 }} src={feature.fields.image} alt="Cat" />}</Link>
+          <h2>{feature.fields.name}</h2>
+        </div>
+      ))
+    }
+  </Carousel.Item>
+</Carousel> */}
+
+{/* <Carousel varient="dark"
+            key={feature.id}>
+            <Carousel.Item>
+              {/* <div className="cats"> */
+  //   <Link
+  //     to={`/featured/${feature.id}`}>{<img style={{ width: 400, height: 300 }} src={feature.fields.image} alt="Cat" />}</Link>
+  //   <Carousel.Caption>
+  //     <h2>{feature.fields.name}</h2>
+  //   </Carousel.Caption>
+  //   {/* </div> */}
+  // </Carousel.Item>
+  // </Carousel> *
+}
+
