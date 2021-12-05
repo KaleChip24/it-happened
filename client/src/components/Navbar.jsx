2800e5b8219ee33dd17dbb22aa9ee67f94180b96
KaleOHara <92695012+KaleChip24@users.noreply.github.com>
2021-12-05 17:34:16 -0500

    getting closer to the color pallet i want

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,11 +2,12 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <div className="navbar">
+    <nav className="navbar">
+      <img src="./images/movie-ticket" alt="movie ticket" />
       <Link to="/">Home</Link>
       <Link to="/new">Add Purrfect Review</Link>
       <Link to="/featured">Featured Feline</Link>
-    </div>
+    </nav>
   );
 };
 
