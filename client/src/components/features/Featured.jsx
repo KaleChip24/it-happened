b9c04f1ce5049d7db7f49d6e5595287c63d520cd
KaleOHara <92695012+KaleChip24@users.noreply.github.com>
2021-12-05 11:46:26 -0500

    deleted junk commented out code

--- client/src/components/features/Featured.jsx
+++ client/src/components/features/Featured.jsx
@@ -5,7 +5,7 @@ import Carousel from 'react-bootstrap/Carousel';
 function Featured(props, id) {
 
   return (
-    <div>
+    <div className="carousel-wrap">
       {props.features.length > 1 ?
         <Carousel> {
           props.features.map((feature, id) => (
@@ -31,34 +31,4 @@ function Featured(props, id) {
 }
 
 export default Featured;
-
-
-
-{/* <Carousel variant="dark">
-  <Carousel.Item>
-    {
-      props.features.map(feature => (
-        <div className="cats" key={feature.id}>
-          <Link
-            to={`/featured/${feature.id}`}>{<img style={{ width: 400, height: 300 }} src={feature.fields.image} alt="Cat" />}</Link>
-          <h2>{feature.fields.name}</h2>
-        </div>
-      ))
-    }
-  </Carousel.Item>
-</Carousel> */}
-
-{/* <Carousel varient="dark"
-            key={feature.id}>
-            <Carousel.Item>
-              {/* <div className="cats"> */
-  //   <Link
-  //     to={`/featured/${feature.id}`}>{<img style={{ width: 400, height: 300 }} src={feature.fields.image} alt="Cat" />}</Link>
-  //   <Carousel.Caption>
-  //     <h2>{feature.fields.name}</h2>
-  //   </Carousel.Caption>
-  //   {/* </div> */}
-  // </Carousel.Item>
-  // </Carousel> *
-}
 
