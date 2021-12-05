763f954ebd298d52c54d569ffff3f7cea3866ffb
KaleOHara <92695012+KaleChip24@users.noreply.github.com>
2021-12-05 11:38:24 -0500

    carousel works woot woot

--- client/src/components/features/Featured.jsx
+++ client/src/components/features/Featured.jsx
@@ -2,18 +2,29 @@ import { Link } from 'react-router-dom'
 import { useEffet, useState } from 'react'
 import Carousel from 'react-bootstrap/Carousel';
 
-function Featured(props) {
+function Featured(props, id) {
 
   return (
     <div>
-      {
-        props.features.map(feature => (
-          <div className="cats" key={feature.id}>
-            <Link
-              to={`/featured/${feature.id}`}>{<img style={{ width: 400, height: 300 }} src={feature.fields.image} alt="Cat" />}</Link>
-            <h2>{feature.fields.name}</h2>
-          </div>
-        ))
+      {props.features.length > 1 ?
+        <Carousel> {
+          props.features.map((feature, id) => (
+            <Carousel.Item key={feature.id}>
+              <div className="cats" key={feature.id}>
+                <Link
+                  to={`/featured/${feature.id}`}>{<img style={{ width: 400, height: 300 }} src={feature.fields.image} alt="Cat" />}</Link>
+                <Carousel.Caption>
+                  <h2>{feature.fields.name}</h2>
+                </Carousel.Caption>
+              </div>
+            </Carousel.Item>
+          ))
+        }
+        </Carousel>
+        :
+        <div className="features-image">
+          <img style={{ width: 400, height: 300 }} src={props.features.fields.image} alt="Cat" />
+        </div>
       }
     </div>
   );
