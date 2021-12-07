b6cc4a4707ac469f53d56a64e8efd173cc54b609
KaleOHara <92695012+KaleChip24@users.noreply.github.com>
2021-12-07 16:04:48 -0500

    featured comments are in their own little blocks, still needs some work

--- client/src/components/Films.jsx
+++ client/src/components/Films.jsx
@@ -2,6 +2,8 @@ import { Link } from 'react-router-dom'
 
 
 function Films(props) {
+
+
   return (
     <div className="home-posters">
       {
--- client/src/components/features/Comment.jsx
+++ client/src/components/features/Comment.jsx
@@ -1,34 +1,37 @@
 import React from 'react';
 import { useParams, useNavigate } from 'react-router-dom'
 import { deleteComments } from '../../services/index'
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import { Container } from 'react-bootstrap';
 
 function Comment(props) {
   const params = useParams()
   const nav = useNavigate()
 
-  const handleDelete = async () => {
-    const res = await deleteComments(params.id)
-    props.setToggle(e => !e)
-    if (res) {
-      nav(`/featured`)
-    }
-  }
-  return (
-    <div className="comment-box">
-      {
-        props.comment.map(comments => (
-          <div key={comments.id} className="comment-data">
-            <h3>{comments.fields.name}</h3>
-            <p>{comments.fields.comments}</p>
-            <button onClick={handleDelete}>DELETE</button>
-          </div>
-        ))
-      }
+  // const handleDelete = async () => {
+  //   const res = await deleteComments(params.id)
+  //   props.setToggle(e => !e)
+  //   if (res) {
+  //     nav(`/featured`)
+  //   }
 
-    </div>
+  return (
+    <Container>
+      <Grid container>
+        {props.comment.map(comments => (
+          <Grid item key={comments.id} className="comment-data" xs={12} md={6} lg={4}>
+            <Paper>
+              <p>{comments.fields.comments}</p>
+              <h3>{comments.fields.name}</h3>
+              {/* <button onClick={handleDelete}>DELETE</button> */}
+            </Paper>
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
   );
 }
 
 export default Comment;
 
-// props.comment && props.comment.fields &&
